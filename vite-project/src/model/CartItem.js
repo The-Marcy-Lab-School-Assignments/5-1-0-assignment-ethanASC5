df5d65eedc330b8d31dddda8fba47e02cea62d37
Marcy <marcy@Marcys-MacBook-Pro-4.local>
2024-08-09 19:55:50 -0400

    finished

--- vite-project/src/model/CartItem.js
+++ vite-project/src/model/CartItem.js
@@ -1,7 +1,11 @@
 import getId from "../utils/getId";
 
 class CartItem {
-
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+    this.id = getId();
+  }
 }
 
 export default CartItem;
--- vite-project/src/model/ShoppingCart.js
+++ vite-project/src/model/ShoppingCart.js
@@ -2,7 +2,43 @@ import getId from "../utils/getId";
 import CartItem from "./CartItem";
 
 class ShoppingCart {
+  #cartItems = [];
+  static #allCarts = [];
 
+  constructor() {
+    this.id = getId();
+
+    ShoppingCart.#allCarts.push(this);
+  }
+
+  createItem(name, price) {
+    const addedItem = new CartItem(name, price);
+    this.#cartItems.push(addedItem);
+    return addedItem;
+  }
+
+  getItems() {
+    return [...this.#cartItems];
+  }
+
+  removeItem(id) {
+    this.#cartItems.splice(
+      this.#cartItems.findIndex((item) => item.id === id),
+      1
+    );
+  }
+
+  getTotal() {
+    return this.#cartItems.reduce((acc, { price }) => acc + price, 0);
+  }
+
+  static listAll() {
+    return [...this.#allCarts];
+  }
+
+  static findBy(searchTerm) {
+    return ShoppingCart.#allCarts.find(({ id }) => id === searchTerm);
+  }
 }
 
 export default ShoppingCart;
